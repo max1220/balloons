1fdaf4d1b75faea995e838c96cc3b14a4ae3409b
max1220 <max@big-gentoo.lan>
2016-06-14 17:39:51 +0200

    Moved configuration to config file

---
+++ config.js
@@ -0,0 +1,18 @@
+var config = {
+  spacing_leftright: 64,
+  normal_spawn_min: 450,
+  normal_spawn_max: 550,
+  logo_scale: 4,
+  max_lifes: 5,
+  heart_scale: 4,
+  // try_again_scale: 4,
+  balloon_colors: [ "red", "lightblue", "darkblue", "grey", "lime", "orange", "purple", "wine", "yellow" ],
+  balloon_sprite_width: 24,
+  balloon_sprite_height: 36,
+  balloon_sprite_count: 5,
+	speed_accel: 1/10000000,
+	speed_max: 0.5,
+	debug_mode: false,
+	last_username: "Unnamed",
+	highscore_url: "/cgi-bin/highscore.lua" // The URL we post the score to, and redirect on the highscore link
+};
